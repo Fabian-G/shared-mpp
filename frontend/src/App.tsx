9a6b4e2e0f1b81a9a0b4d5dc1d53e27ffacf1725
Fabian G <fabian_g@mailbox.org>
2022-01-31 12:37:44 +0100

    Use different clients

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,21 @@
 import React, {useMemo, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import commonClient from "common-client"
 import aClient from "a-client"
 import bClient from "b-client"
 
-const createClient = commonClient.org.example.commonclient.createClient;
+const createClientA = aClient.org.example.commonclient.createClient;
+const createClientB = bClient.org.example.commonclient.createClient;
 const JsTodosApi = aClient.org.example.aclient.JsTodosApi
 type Todo = aClient.org.example.aclient.Todo
 const JsPostsApi = bClient.org.example.bclient.JsPostsApi
 type Post = bClient.org.example.bclient.Post
 
 function App() {
-    const client = useMemo(() => createClient("https://jsonplaceholder.typicode.com"), [])
-    const todoApi = useMemo(() => new JsTodosApi(client), [client])
-    const postsApi = useMemo(() => new JsPostsApi(client), [client])
+    const clientA = useMemo(() => createClientA("https://jsonplaceholder.typicode.com"), [])
+    const clientB = useMemo(() => createClientB("https://jsonplaceholder.typicode.com"), [])
+    const todoApi = useMemo(() => new JsTodosApi(clientA), [clientA])
+    const postsApi = useMemo(() => new JsPostsApi(clientB), [clientB])
 
     const [post, setPost] = useState<Post | null>(null)
     const [todo, setTodo] = useState<Todo | null>(null)
